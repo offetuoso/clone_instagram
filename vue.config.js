7b74b92b64cb3ec446dc329a2de0653a7392aaa4
offetuoso <offetuoso@gmail.com>
2021-12-07 00:27:44 +0900

    2021-12-07-1

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const path = require("path");
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "/clone_instagram/" : "/",
+    outputDir: path.resolve(__dirname, "./dist"),
+
+    //   assetsDir: "./"
+    //   asset 파일 위치 설정
 };
